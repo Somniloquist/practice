b5020186444bf77bfa6e3b8191c6bc6426b5dbfa
Robin Unger <robin.e.unger@gmail.com>
2022-10-18 21:25:18 -0500

    added substrings.rb

---
+++ substrings.rb
@@ -0,0 +1,19 @@
+require "pry-byebug"
+
+def substrings(phrase, dictionary)
+  phrase = phrase.split()
+  results = Hash.new(0)
+
+  for word in phrase
+    for substring in dictionary
+      word.include?(substring) ? results[substring] += 1 : results
+    end
+  end
+
+  results
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
